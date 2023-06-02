a925f4ed6eeb7dcbddf6cc771aaa68e4b6040377
Oliver Carmont <ocarmont@berkeley.edu>
2023-06-01 20:49:21 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='dashboard'>
+        <div className='header'>
+          <h1>Welcome to Your Dashboard</h1>
+        </div>
+        <div className='content'>
+          <div className='sidebar'>
+            <h2>Menu</h2>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+          <div className='main'>
+            <h2>Main Content</h2>
+            <p>This is where your main content goes.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Dashboard />
     </div>
   );
 }
